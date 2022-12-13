54d2beaa9ab2c3e11a20d91a95505c4f852f81aa
Ghoul12342 <114170053+Ghoul12342@users.noreply.github.com>
2022-12-13 18:28:39 +0300

    Add files via upload

---
+++ functions.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "SFML/Graphics.hpp"
+#include "settings.h"
+
+void initBat(sf::RectangleShape& bat, const sf::Color batColor, const sf::Vector2f batPosition)
+{
+	bat.setSize(batSize);
+	bat.setFillColor(batColor);
+	bat.setPosition(batPosition);
+}
+
+void initBat(sf::RectangleShape& bat, const sf::Color batColor, const sf::Vector2f batPosition)
+{
+	bat.setSize(batSize);
+	bat.setFillColor(batColor);
+	bat.setPosition(batPosition);
+}
